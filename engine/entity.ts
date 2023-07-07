066cc8e1b50c7b3a4b652c7acef7795dd4afa317
Richard Smely <smelyric@fit.cvut.cz>
2023-07-07 22:06:59 +0200

    .

--- engine/entity.ts
+++ engine/entity.ts
@@ -1,10 +1,26 @@
-import { vec3 } from "gl-matrix";
+import { vec3, mat4 } from "gl-matrix";
 import { Model } from "./drawable";
 
 export class Transform {
     position = vec3.create();
     rotation = vec3.create();
     scale = vec3.create();
+
+    GetTranslationMatrix() {
+
+    }
+
+    GetRotationMatrix() {
+
+    }
+
+    GetScaleMatrix() {
+
+    }
+
+    GetTransformMatrix() {
+
+    }
 };
 
 export class Entity {
